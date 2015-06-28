67f20afdce8281d80354e06fe3de38d85f58c72a
Bruno Paulin <brunopaulin@bpaulin.net>
2015-06-28 15:44:14 +0200

    CS: events test

--- src/Tests/Event/FieldAttributeEventTest.php
+++ src/Tests/Event/FieldAttributeEventTest.php
@@ -11,20 +11,20 @@ class FieldAttributeEventTest extends \PHPUnit_Framework_TestCase
         $event = new FieldAttributeEvent();
         $this->assertSame(
             $event,
-            $event->setOldValue('foo')
+            $event->setOldValue( 'foo' )
         );
         $this->assertSame(
             'foo',
             $event->getOldValue()
         );
     }
-    
+
     public function testNewValue()
     {
         $event = new FieldAttributeEvent();
         $this->assertSame(
             $event,
-            $event->setNewValue('foo')
+            $event->setNewValue( 'foo' )
         );
         $this->assertSame(
             'foo',
--- src/Tests/Event/ImportEventTest.php
+++ src/Tests/Event/ImportEventTest.php
@@ -11,33 +11,33 @@ class ImportEventTest extends \PHPUnit_Framework_TestCase
         $event = new ImportEvent();
         $this->assertSame(
             $event,
-            $event->setName('foo')
+            $event->setName( 'foo' )
         );
         $this->assertSame(
             'foo',
             $event->getName()
         );
     }
-    
+
     public function testStatus()
     {
         $event = new ImportEvent();
         $this->assertSame(
             $event,
-            $event->setStatus('foo')
+            $event->setStatus( 'foo' )
         );
         $this->assertSame(
             'foo',
             $event->getStatus()
         );
     }
-    
+
     public function testObject()
     {
         $event = new ImportEvent();
         $this->assertSame(
             $event,
-            $event->setObject('foo')
+            $event->setObject( 'foo' )
         );
         $this->assertSame(
             'foo',
